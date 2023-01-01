c0394a3235ec3d72c9ce232e4fa52a9ed3c05db7
Okunade sherif <o.d.alsherif@gmail.com>
2023-01-01 05:45:27 +0100

    added vatebra projects

--- src/Projects/data.js
+++ src/Projects/data.js
@@ -4,9 +4,30 @@ import university from './portfolio_img/alicia.png';
 import vividioz from './portfolio_img/vivioz.PNG';
 import vidly from './portfolio_img/vidly.png';
 import country from './portfolio_img/countriesqq.PNG';
+import vatdoc from '../assets/vatdoc.PNG';
+import verifier from '../assets/verifier.PNG';
+import itemify from '../assets/itemify.PNG'
 
 
 export const data =[
+    {
+        title: 'Vatdoc',
+        demo: 'https://sage-duckanoo-db10a2.netlify.app/',
+        github: 'https://github.com/Lawdshev/Vatdoc',
+        img: vatdoc,
+    },
+    {
+        title: 'Verifier',
+        demo: 'https://iridescent-speculoos-a7e6ea.netlify.app/',
+        github: 'https://github.com/Lawdshev/Everifyer',
+        img: verifier,
+    },
+    {
+        title: 'Itemify',
+        demo: 'https://cosmic-dolphin-db8c7a.netlify.app/',
+        github: 'https://github.com/Lawdshev/Itemify',
+        img: itemify,
+    },
     {
         title: 'Shoklin',
         demo: 'https://shoklin.onrender.com',
