5f4321b3c68db3bfb294c0a915e82e479d33feb7
Okunade sherif <o.d.alsherif@gmail.com>
2023-02-23 19:39:50 +0100

    updated cv

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CV from '../../assets/Okunade Sherif D. CV.pdf'
+import CV from '../../assets/Okunade Sherif Damilare(web dev).pdf'
 
 const CTA = () => {
   return (
